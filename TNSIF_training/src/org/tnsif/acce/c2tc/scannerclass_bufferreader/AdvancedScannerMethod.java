5c65d63e2b226b89af5156d4a5beec737ee7327c
srinc <srinc@Inchara>
2026-08-05 10:49:16 +0530

    first commit

---
+++ TNSIF_training/src/org/tnsif/acce/c2tc/scannerclass_bufferreader/AdvancedScannerMethod.java
@@ -0,0 +1,34 @@
+package org.tnsif.acce.c2tc.scannerclass_bufferreader;
+
+
+	import java.util.Scanner;
+
+	public class AdvancedScannerMethod {
+
+		public static void main(String[] args) {
+			
+
+			        Scanner sc = new Scanner(System.in);
+
+			        System.out.print("Enter any value: ");
+
+			        if (sc.hasNext()) {
+			            System.out.println("Input is available.");
+
+			            if (sc.hasNextInt()) {
+			                int num = sc.nextInt();
+			                System.out.println("You entered an integer: " + num);
+			            } else {
+			                String str = sc.next();
+			                System.out.println("You entered a string: " + str);
+			            }
+			        } else {
+			            System.out.println("No input available.");
+			        }
+
+			        sc.close();
+			    }
+
+	}
+
+
