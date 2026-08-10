cf30f50360535ee9389f0592024cc4c0478b94f5
srinc <srinc@Inchara>
2026-08-10 10:36:32 +0530

    first commit

---
+++ TNSIF_training/src/org/tnsif/acce/c2tc/interfaceDemo/SmartDevice.java
@@ -0,0 +1,14 @@
+package org.tnsif.acce.c2tc.interfaceDemo;
+
+public interface SmartDevice {
+		
+		public abstract void turnOn();
+		public abstract  void turnOff();
+		public abstract  void getStatus();
+		
+		
+		
+
+	}
+
+
